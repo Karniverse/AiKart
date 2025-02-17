a52fa80a62871f174bb92cc45c841477815436c8
mukil-bluerider <mrmukil1991@gmail.com>
2025-02-17 22:53:42 +0530

    Update

--- sourcecode/App.svelte
+++ sourcecode/App.svelte
@@ -2,11 +2,12 @@
 	import IntroCard from "./IntroCard.svelte";
 	import Gallery from "./Gallery.svelte";
 	import SearchBox from "./SearchBox.svelte";
-	// import Test from "./TEMP/test.svelte";
+	import Test from "./TEMP/test.svelte";
 </script>
 
 <div>
 	<IntroCard />
 	<SearchBox />
-	<Gallery />
+	<!-- <Gallery /> -->
+	<Test />
 </div>
